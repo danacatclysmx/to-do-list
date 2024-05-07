02c327bf2a096108c2cd51e0aa925c852a4b2609
Dana Romero <cata0614@gmail.com>
2024-05-07 13:46:26 -0500

    agrego enumeracion de lista,y que no repita tarea

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,30 @@
 const saveItemButton = document.querySelector("#btnSave");
 const inputTask = document.querySelector("#newTask");
 const listGroup = document.querySelector(".list-group");
-saveItemButton.addEventListener("click", (e) => {
-  addNewItem();
-});
+let taskCounter = 1;
+saveItemButton.addEventListener("click", addNewItem);
 inputTask.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     addNewItem();
   }
 });
+
 function addNewItem() {
   const task = inputTask.value.trim(); //quita espacios en blanco al final
-  if (task) {
+  if (task && !noRepetida(task)) {
     const item = document.createElement("a");
     item.href = "#";
     item.className =
       "list-group-item d-flex justify-content-between align-items-center";
-    item.innerHTML = `<span>${task}</span>
-      <div class="">
-        <button type="button" class="btn btn-success btn-sm">
-          <i class="bi bi-check-lg"></i>
-        </button>
-        <button type="button" class="btn btn-danger btn-sm">
-          <i class="bi bi-x-lg"></i>
-        </button>
-      </div>`;
+    item.innerHTML = `<span>${taskCounter}.${task}</span>
+    <div class="">
+      <button type="button" class="btn btn-success btn-sm">
+        <i class="bi bi-check-lg"></i>
+      </button>
+      <button type="button" class="btn btn-danger btn-sm">
+        <i class="bi bi-x-lg"></i>
+      </button>
+    </div>`;
     item.querySelector(".btn-success").addEventListener("click", (e) => {
       item.classList.toggle("list-group-item-success"); // si no esta la clase la crea y si esta la quita
     });
@@ -33,12 +33,19 @@ function addNewItem() {
     });
     listGroup.appendChild(item);
     inputTask.value = "";
+    taskCounter++;
   } else {
     return;
   }
 }
-//homework
-// enuerar la lista de tareas
-// que la tarea nno se pueda repetir
-// poder agrgar una imagen desde una url
-// poder agregar fecha limite.
+function noRepetida(task) {
+  const items = listGroup.querySelectorAll("a");
+  for (let item of items) {
+    const span = item.querySelector("span");
+    if (span.textContent.substring(2) === task) {
+      alert("La tarea ya existe en la lista.");
+      return true;
+    }
+  }
+  return false;
+}
